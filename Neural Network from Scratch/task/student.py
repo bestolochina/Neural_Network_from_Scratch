Stage 3. Incorrect backpropagation values. Check your backpropagation function.

Neural Network from Scratch/task/student.py
@@ -76,6 +76,13 @@ if __name__ == '__main__':
 
     # write your code here
 
+    arr1, arr2 = np.array([-1,0,1,2]), np.array([4,3,2,1])
+    mse = [float(my_module.mse(arr1, arr2))]
+    mse_derivative = my_module.mse_derivative(arr1, arr2).tolist()
+    sigmoid_derivative = my_module.sigmoid_derivative(arr1).tolist()
+
+    print(mse, mse_derivative, sigmoid_derivative, end=' ')
+
     # Use scale to rescale X_train and X_test;
     X_train_rescaled, X_test_rescaled = my_module.scale(X_train, X_test)
     n_samples, n_features = X_train_rescaled.shape
@@ -83,5 +90,9 @@ if __name__ == '__main__':
 
     model = my_module.OneLayerNeural(n_features=n_features, n_classes=n_classes)
 
-    out = model.forward(X_train_rescaled[:2]).flatten().tolist()
-    print(out)
+    model.forward(X=X_train_rescaled[:2])
+    model.backprop(X=X_test_rescaled[:2], y_true=y_train[:2])
+    model.forward(X=X_train_rescaled[:2])
+    error = [float(my_module.mse(y_pred=model.output, y_true=y_train[:2]))]
+
+    print(error)

Neural Network from Scratch/task/test/tests.py
@@ -1,20 +1,18 @@
+import numpy as np
 from hstest import StageTest, TestCase, CheckResult
 from hstest.stage_test import List
 from utils.utils import get_list, full_check, custom_uniform
-
-import numpy as np
 np.random.uniform = custom_uniform
 
 # The source data I will test on
 
-true_forward_res = [0.42286725173173645, 0.7863175754895447, 0.8539526054946633, 0.5878649450449149,
-                    0.25332037818521813, 0.10846218815633141, 0.16132366288535735, 0.5036812915517839,
-                    0.39649108111105275, 0.3378884293704015, 0.418550728613325, 0.7054997389006183,
-                    0.7505531230813576, 0.5719368355036792, 0.14896990554768216, 0.2714895260027178,
-                    0.36007653088357094, 0.5919441786952234, 0.6768202587658813, 0.5221310346087061]
+true_mse_res = [9.0]
+true_mse_der_res = [-10, -6, -2, 2]
+true_sigmoid_der_res = [0.19661193324148185, 0.25, 0.19661193324148185, 0.10499358540350662]
+true_backprop_res = [0.027703041616827684]
 
 
-class Tests2(StageTest):
+class Tests3(StageTest):
 
     def generate(self) -> List[TestCase]:
         return [TestCase(time_limit=1000000)]
@@ -25,15 +23,41 @@ class Tests2(StageTest):
         if len(reply) == 0:
             return CheckResult.wrong("No output was printed")
 
-        if reply.count('[') != 1 or reply.count(']') != 1:
+        if reply.count('[') != 4 or reply.count(']') != 4:
             return CheckResult.wrong('No expected lists were found in output')
 
-        try:
-            forward_res, _ = get_list(reply)
-        except Exception:
-            return CheckResult.wrong('Seems that feedforward output is in wrong format')
+        # Getting the student's results from the reply
 
-        check_result = full_check(forward_res, true_forward_res, 'feedforward')
+        try:
+            mse_res, reply = get_list(reply)
+        except Exception:
+            return CheckResult.wrong('Seems that MSE output is in wrong format')
+
+        try:
+            mse_der_res, reply = get_list(reply)
+        except Exception:
+            return CheckResult.wrong('Seems that MSE derivative output is in wrong format')
+
+        try:
+            sigmoid_der_res, reply = get_list(reply)
+        except Exception:
+            return CheckResult.wrong('Seems that sigmoid derivative output is in wrong format')
+
+        try:
+            backprop_res, _ = get_list(reply)
+        except Exception:
+            return CheckResult.wrong('Seems that backpropagation output is in wrong format')
+
+        check_result = full_check(mse_res, true_mse_res, 'MSE')
+        if check_result:
+            return check_result
+        check_result = full_check(mse_der_res, true_mse_der_res, 'MSE derivative')
+        if check_result:
+            return check_result
+        check_result = full_check(sigmoid_der_res, true_sigmoid_der_res, 'sigmoid derivative')
+        if check_result:
+            return check_result
+        check_result = full_check(backprop_res, true_backprop_res, 'backpropagation')
         if check_result:
             return check_result
 
@@ -41,4 +65,4 @@ class Tests2(StageTest):
 
 
 if __name__ == '__main__':
-    Tests2().run_tests()
+    Tests3().run_tests()